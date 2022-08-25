a312293e4a304d12b973b55143749ede28d6c85e
Madan Bajgai <madanbajgai@gmail.com>
2022-08-25 18:56:54 +0545

    Under Construction

--- readme.md
+++ readme.md
@@ -1,8 +1,1 @@
-# Todos of the project
-
-1. modular code with templates/partials
-2. darkmode
-3. pwa
-4. Animations
-5. hover & focus effects
-6. Code Refactoring
+# Page Under Construction
--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".burger");
-const links = document.querySelector("ul");
-const nav = document.querySelector("nav");
-
-burger.addEventListener("click", () => {
-  links.classList.toggle("active");
-  nav.classList.toggle("active");
-});
