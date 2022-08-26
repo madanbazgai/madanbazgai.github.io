d3caceb582db5459cedbc40a3a659f82859d7730
Madan Bajgai <madanbajgai@gmail.com>
2022-08-26 18:35:07 +0545

    pwa support

---
+++ sw.js
@@ -0,0 +1,50 @@
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+const OFFLINE_URL = ["index.html", "/assets/woff.woff2", "/assets/favicon.ico"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    //caching static assets
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(OFFLINE_URL);
+    })
+  );
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
+});
