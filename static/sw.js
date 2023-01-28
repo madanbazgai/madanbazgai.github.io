2b9daeea8a448a6b1fae992cb0fe46d07e6e4886
Madan Bajgai <madanbajgai@gmail.com>
2023-01-28 23:13:14 +0545

    ..

--- static/sw.js
+++ static/sw.js
@@ -13,49 +13,41 @@ const urlsToCache = [
   "madmax.avif",
 ];
 
-// Add event listener for 'install' event
 self.addEventListener("install", (event) => {
-  // Wait until the cache is created
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      // Add all URLs to cache
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Add event listener for 'fetch' event
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches
       .match(event.request)
       .then((response) => {
-        // If the request is already in cache, return it
         if (response) {
           return response;
         }
-        // Otherwise, fetch the request and add it to cache
-        else {
-          // Clone request stream
-          var fetchRequest = event.request.clone();
 
-          return fetch(fetchRequest).then((response) => {
-            // If the response is not valid, return 404 page
-            if (!response || response.status !== 200) {
-              return caches.match("/404.html");
-            }
-            // Otherwise, clone response stream and add it to cache
-            else {
-              var responseToCache = response.clone();
-              caches.open(CACHE_NAME).then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-              return response;
-            }
+        // clone request stream
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then((response) => {
+          if (!response || response.status !== 200) {
+            return caches.match("/404.html");
+          }
+
+          // clone response stream
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
           });
-        }
+
+          return response;
+        });
       })
-      // If there is an error, return 404 page
       .catch(() => {
         return caches.match("/404.html");
       })
