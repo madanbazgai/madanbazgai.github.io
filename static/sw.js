94b26b34ca925aeca57dcc86178e8ca59097b4c8
Madan Bajgai <madanbajgai@gmail.com>
2023-04-28 17:47:06 +0545

    added image caching

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js"
 );
 
-const { StaleWhileRevalidate } = workbox.strategies;
+const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
 const { precacheAndRoute } = workbox.precaching;
 
 precacheAndRoute([{ url: "/404.html", revision: null }]);
@@ -10,11 +10,31 @@ precacheAndRoute([{ url: "/404.html", revision: null }]);
 workbox.routing.registerRoute(
   ({ request }) => {
     return (
-      request.destination === "style" || request.destination === "document"
+      request.destination === "style" ||
+      request.destination === "document" ||
+      request.destination === "image"
     );
   },
   new StaleWhileRevalidate({
     cacheName: "my-cache",
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => {
+    return request.destination === "image";
+  },
+  new CacheFirst({
+    cacheName: "image-cache",
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200],
